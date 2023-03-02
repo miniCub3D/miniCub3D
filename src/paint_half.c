9db3c3535b245544022b06b3754542d6d875b886
Seunghun Song <seunghso@c3r5s2.42seoul.kr>
2023-03-02 17:02:52 +0900

    pain_half() 작업 절반으로 감소

--- src/paint_half.c
+++ src/paint_half.c
@@ -9,16 +9,16 @@ void	paint_half(t_game *game)
 	while (x < RESOLUTION_W)
 	{
 		y = 0;
-		while (y < RESOLUTION_H)
+		while (y < RESOLUTION_H / 2)
 		{
 			game->buf[y][x] = \
-			((game->map_info.color_f.r & 0xff) << 16) + \
-			((game->map_info.color_f.g & 0xff) << 8) + \
-			((game->map_info.color_f.b) & 0xff);
-			game->buf[RESOLUTION_H - y - 1][x] = \
 			((game->map_info.color_c.r & 0xff) << 16) + \
 			((game->map_info.color_c.g & 0xff) << 8) + \
 			((game->map_info.color_c.b) & 0xff);
+			game->buf[RESOLUTION_H - y - 1][x] = \
+			((game->map_info.color_f.r & 0xff) << 16) + \
+			((game->map_info.color_f.g & 0xff) << 8) + \
+			((game->map_info.color_f.b) & 0xff);
 			y++;
 		}
 		x++;
