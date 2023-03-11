97a43bdbcd09b78715ee4857a0b52f6953300ded
장현진 <haegu@janghyeonjin-ui-MacBookPro.local>
2023-03-11 14:55:28 +0900

    mingi command file

--- src/command1.c
+++ src/command1.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command1.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkwon <mkwon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 04:29:50 by mkwon             #+#    #+#             */
+/*   Updated: 2023/03/01 04:29:51 by mkwon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3D.h"
 
 void	restore_position(t_game *game, double old_x, double old_y)
@@ -28,6 +40,40 @@ void	restore_position(t_game *game, double old_x, double old_y)
 	}
 }
 
+void	turn_left(t_game *game)
+{
+	double	old_direction_x;
+	double	old_plane_x;
+
+	old_direction_x = game->play_info.dir_x;
+	old_plane_x = game->play_info.plane_x;
+	game->play_info.dir_x = game->play_info.dir_x * cos(0.05) - \
+	game->play_info.dir_y * sin(0.05);
+	game->play_info.dir_y = old_direction_x * sin(0.05) + \
+	game->play_info.dir_y * cos(0.05);
+	game->play_info.plane_x = game->play_info.plane_x * cos(0.05) - \
+	game->play_info.plane_y * sin(0.05);
+	game->play_info.plane_y = old_plane_x * sin(0.05) + \
+	game->play_info.plane_y * cos(0.05);
+}
+
+void	turn_right(t_game *game)
+{
+	double	old_direction_x;
+	double	old_plane_x;
+
+	old_direction_x = game->play_info.dir_x;
+	old_plane_x = game->play_info.plane_x;
+	game->play_info.dir_x = game->play_info.dir_x * cos(-0.05) - \
+	game->play_info.dir_y * sin(-0.05);
+	game->play_info.dir_y = old_direction_x * sin(-0.05) + \
+	game->play_info.dir_y * cos(-0.05);
+	game->play_info.plane_x = game->play_info.plane_x * cos(-0.05) - \
+	game->play_info.plane_y * sin(-0.05);
+	game->play_info.plane_y = old_plane_x * sin(-0.05) + \
+	game->play_info.plane_y * cos(-0.05);
+}
+
 int	key_hook(int key_code, t_game *game)
 {
 	double	old_x;
@@ -37,13 +83,17 @@ int	key_hook(int key_code, t_game *game)
 	old_y = game->play_info.pos_y;
 	if (key_code == KEY_ESC)
 		exit_game(game);
-	if (key_code == KEY_W || key_code == KEY_UP)
+	if (key_code == KEY_W)
 		push_up(game, old_x, old_y);
-	if (key_code == KEY_S || key_code == KEY_DOWN)
+	if (key_code == KEY_S)
 		push_down(game, old_x, old_y);
-	if (key_code == KEY_A || key_code == KEY_LEFT)
-		push_left(game);
-	if (key_code == KEY_D || key_code == KEY_RIGHT)
-		push_right(game);
+	if (key_code == KEY_A)
+		push_left(game, old_x, old_y);
+	if (key_code == KEY_D)
+		push_right(game, old_x, old_y);
+	if (key_code == KEY_LEFT)
+		turn_left(game);
+	if (key_code == KEY_RIGHT)
+		turn_right(game);
 	return (0);
 }
--- src/command2.c
+++ src/command2.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command2.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkwon <mkwon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 04:29:54 by mkwon             #+#    #+#             */
+/*   Updated: 2023/03/01 04:29:56 by mkwon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3D.h"
 
 void	push_up(t_game *game, double old_x, double old_y)
@@ -40,36 +52,42 @@ void	push_down(t_game *game, double old_x, double old_y)
 	restore_position(game, old_x, old_y);
 }
 
-void	push_left(t_game *game)
+void	push_left(t_game *game, double old_x, double old_y)
 {
-	double	old_direction_x;
-	double	old_plane_x;
+	double	future_x;
+	double	future_y;
 
-	old_direction_x = game->play_info.dir_x;
-	old_plane_x = game->play_info.plane_x;
-	game->play_info.dir_x = game->play_info.dir_x * cos(0.05) - \
-	game->play_info.dir_y * sin(0.05);
-	game->play_info.dir_y = old_direction_x * sin(0.05) + \
-	game->play_info.dir_y * cos(0.05);
-	game->play_info.plane_x = game->play_info.plane_x * cos(0.05) - \
-	game->play_info.plane_y * sin(0.05);
-	game->play_info.plane_y = old_plane_x * sin(0.05) + \
-	game->play_info.plane_y * cos(0.05);
+	if (game->map_info.map[(int)(game->play_info.pos_x - \
+		game->play_info.plane_x * 0.1)][(int)(game->play_info.pos_y)] != 1)
+	{
+		future_x = game->play_info.pos_x - game->play_info.plane_x * 0.2;
+		game->play_info.pos_x -= game->play_info.plane_x * 0.1;
+	}
+	if (game->map_info.map[(int)(game->play_info.pos_x)] \
+		[(int)(game->play_info.pos_y - game->play_info.plane_y * 0.1)] != 1)
+	{
+		future_y = game->play_info.pos_y - game->play_info.plane_y * 0.2;
+		game->play_info.pos_y -= game->play_info.plane_y * 0.1;
+	}
+	restore_position(game, old_x, old_y);
 }
 
-void	push_right(t_game *game)
+void	push_right(t_game *game, double old_x, double old_y)
 {
-	double	old_direction_x;
-	double	old_plane_x;
+	double	future_x;
+	double	future_y;
 
-	old_direction_x = game->play_info.dir_x;
-	old_plane_x = game->play_info.plane_x;
-	game->play_info.dir_x = game->play_info.dir_x * cos(-0.05) - \
-	game->play_info.dir_y * sin(-0.05);
-	game->play_info.dir_y = old_direction_x * sin(-0.05) + \
-	game->play_info.dir_y * cos(-0.05);
-	game->play_info.plane_x = game->play_info.plane_x * cos(-0.05) - \
-	game->play_info.plane_y * sin(-0.05);
-	game->play_info.plane_y = old_plane_x * sin(-0.05) + \
-	game->play_info.plane_y * cos(-0.05);
+	if (game->map_info.map[(int)(game->play_info.pos_x + \
+		game->play_info.plane_x * 0.1)][(int)(game->play_info.pos_y)] != 1)
+	{
+		future_x = game->play_info.pos_x + game->play_info.plane_x * 0.2;
+		game->play_info.pos_x += game->play_info.plane_x * 0.1;
+	}
+	if (game->map_info.map[(int)(game->play_info.pos_x)] \
+		[(int)(game->play_info.pos_y + game->play_info.plane_y * 0.1)] != 1)
+	{
+		future_y = game->play_info.pos_y + game->play_info.plane_y * 0.2;
+		game->play_info.pos_y += game->play_info.plane_y * 0.1;
+	}
+	restore_position(game, old_x, old_y);
 }
